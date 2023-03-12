e7744ec1114d8b16aac26ff02519086edb36316e
Honya <HonyaDev@gmail.com>
2023-03-12 14:49:27 -0500

    rearrange: move frontend code to own folder

---
+++ honya-frontend/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
+mod components;
+
+use components::carousel::Carousel;
+use components::cabinet::BookCase;
+
+fn main() {
+    // init debug tool for WebAssembly
+    wasm_logger::init(wasm_logger::Config::default());
+    console_error_panic_hook::set_once();
+
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        div {
+            class: "container mx-auto",
+            Carousel {},
+            BookCase {},
+        }
+    ))
+}
